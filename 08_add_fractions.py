72df3bbce9ea7a446e5ee6ef15037772e652d986
Nitesh-13 <niteshmali132002@gmail.com>
2024-04-05 00:12:07 +0530

    Coding Practice Day 2 (Logic Fix)

--- 08_add_fractions.py
+++ 08_add_fractions.py
@@ -6,17 +6,13 @@ Program to add two fractions
 Add two fraction a/b and c/d and print answer in simplest form.
 
 '''
-
 import math
+
 n1 = int(input())
 d1 = int(input())
 n2 = int(input())
 d2 = int(input())
-
-lcm = math.lcm(d1,d2)
-
-do = lcm
-no = min(n1,n2)*lcm + max(n1,n2)
-print(f'{no}/{do}')
-
-# Need Logic Fixing
+n = n1*d2+n2*d1
+d = d1*d2
+gcd = math.gcd(n,d) # lcm = (a*b)/gcd
+print(f'{int(n/gcd)}/{int(d/gcd)}')
