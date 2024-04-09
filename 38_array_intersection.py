0ee61b61375ecb382258e61bd54ac9142a935921
Nitesh-13 <niteshmali132002@gmail.com>
2024-04-10 00:14:53 +0530

    Coding Practice Day 10 (Optimized Logic)

--- 38_array_intersection.py
+++ 38_array_intersection.py
@@ -9,5 +9,12 @@ Given two integer arrays nums1 and nums2, return an array of their intersection.
 nums1 = [4,9,5]
 nums2 = [9,4,9,8,4]
 
-out = list(set(filter(lambda x: nums2.count(x)>0, nums1)))
+# O(N^2) Approach
+# out = list(set(filter(lambda x: nums2.count(x)>0, nums1)))
+# print(out)
+
+# O(N) space and time Approach
+set1 = set(nums1)
+set2 = set(nums2)
+out = list(set1.intersection(set2))
 print(out)
