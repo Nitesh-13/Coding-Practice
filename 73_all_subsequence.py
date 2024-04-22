7281213ac418a693eca5d037270a47182c486aa2
Nitesh-13 <niteshmali132002@gmail.com>
2024-04-23 00:01:39 +0530

    Coding Practice Day 19 (Subsequence for lists)

--- 73_all_subsequence.py
+++ 73_all_subsequence.py
@@ -22,3 +22,19 @@ def getSubSequence(string, output):
 subsequences = getSubSequence(string,"")
 print(len(subsequences))
 print((2**len(string))-1) # Formula for no. of possible subsequences (2^n-1)
+
+# If we need string subsequences like 'hel', 'hello', pass '' as output
+# If we need list as subseqquence like  ['l', 'o'], ['l'], pass [] as output
+# If list, modify the sequence[0] to be in the list aswell
+
+
+# Subsequence with their indexes
+def getSubSequenceWithIndices(sequence, output, index = 0):
+    
+    if len(sequence) == 0:
+        return [output] if output else []
+    
+    sub_with_char = getSubSequenceWithIndices(sequence[1:], output + [(index,sequence[0])], index+1) # Changes here
+    sub_without_char = getSubSequenceWithIndices(sequence[1:], output, index+1)
+    
+    return sub_with_char+sub_without_char
